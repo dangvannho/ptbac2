68349cafe8a7e05ca09924ca446697c17f4245c8
dangvannho <dangvannho7@gmail.com>
2023-11-10 21:28:15 +0700

    upload lan 4

--- main.js
+++ main.js
@@ -24,12 +24,6 @@ document
       return;
     }
 
-    if (a === 0) {
-      document.getElementById("result").innerText =
-        "Đây không phải là phương trình bậc hai.";
-      return;
-    }
-
     const delta = b * b - 4 * a * c;
     let result;
 
